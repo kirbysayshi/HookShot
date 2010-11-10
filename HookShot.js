86f3c74974709cc8d2361dd45701e910e03bf8df
Drew Petersen <kirbysayshi@gmail.com>
2010-11-09 19:07:31 -0500

    converted tabs to spaces... KOMODO!

--- HookShot.js
+++ HookShot.js
@@ -3,18 +3,18 @@ function HookShot(data, isHead){
     this.next;
     this.prev;
     this.data = data;
-	
-	if(isHead === true || isHead == undefined){ 
-		this.head = this; 
-		this.length = 1;
-	}
+    
+    if(isHead === true || isHead == undefined){ 
+        this.head = this; 
+        this.length = 1;
+    }
 }
 
 HookShot.prototype.length = function(){
-	return this.head.length;
+    return this.head.length;
 };
 HookShot.prototype.push = function(data){
-	var node = new HookShot(data, false);
+    var node = new HookShot(data, false);
     this.next = node;
     node.prev = this;
     node.head = this.head;
@@ -22,7 +22,7 @@ HookShot.prototype.push = function(data){
     return node;
 }
 HookShot.prototype.link = function(link){
-	this.next = link;
+    this.next = link;
     link.prev = this;
     link.head = this.head;
     this.head.length++;
@@ -31,21 +31,21 @@ HookShot.prototype.link = function(link){
 HookShot.prototype.detach = function(){
     var  prev = this.prev
         ,next = this.next
-		,length = this.head.length;
+        ,length = this.head.length;
     
-	// special case: reassign all the heads to the next in line...
-	if(this.head == this && next != undefined){
-		this.forEach(function(){
-			this.head = next;
-		});
-	}
-	
-	if(prev != undefined && prev.next == this) { prev.next = next; }
-	if(next != undefined && next.prev == this) { next.prev = prev; }
-	this.head.length = length-1;
-	
-	// set head to this and return
-	return this.head = this;
+    // special case: reassign all the heads to the next in line...
+    if(this.head == this && next != undefined){
+        this.forEach(function(){
+            this.head = next;
+        });
+    }
+    
+    if(prev != undefined && prev.next == this) { prev.next = next; }
+    if(next != undefined && next.prev == this) { next.prev = prev; }
+    this.head.length = length-1;
+    
+    // set head to this and return
+    return this.head = this;
 }
 HookShot.prototype.get = function(i){
     var current = this.head
@@ -58,7 +58,7 @@ HookShot.prototype.get = function(i){
         count++;
         current = current.next;
     }
-	return this;
+    return this;
 }
 HookShot.prototype.forEach = function(callback){
     var current = this.head
@@ -66,62 +66,60 @@ HookShot.prototype.forEach = function(callback){
 
     while(current){       
         if(callback.call(current, count) === false){
-			break;
-		}
+            break;
+        }
         count++;
         current = current.next;
     }
-	return this;
+    return this;
 }
 HookShot.prototype.clone = function(){
-	var current = new HookShot(this.data);
-	this.forEach(function(index){
-		if(index > 0){
-			current = current.push(this.data);
-		}
-	});
-	return current;
+    var current = new HookShot(this.data);
+    this.forEach(function(index){
+        if(index > 0){
+            current = current.push(this.data);
+        }
+    });
+    return current;
 }
 HookShot.prototype.where = function(conditionCallback, mutate){
-	var  news = []
-		,newHook = new HookShot()
-		,i = 0
-		,l = 0;
-	
-	this.forEach(function(index){
-		if( conditionCallback.call(this, index) === true ){
-			if(mutate === false || mutate == undefined){
-				news.push( this.data );
-			} else {
-				news.push( this.detach() );
-			}
-		}
-	});
-	
-	if(news.length > 0){
-		if(mutate === false || mutate == undefined){
-			newHook = new HookShot(news[0]);
-			for(i = 1, l = news.length; i < l; i++){
-				newHook = newHook.push( news[i] );
-			}
-		} else {
-			newHook = news[0];
-			for(i = 1, l = news.length; i < l; i++){
-				newHook = newHook.link( news[i] );
-			}
-		}
-	}
-	
-	return newHook;
+    var  news = []
+        ,newHook = new HookShot()
+        ,i = 0
+        ,l = 0;
+    
+    this.forEach(function(index){
+        if( conditionCallback.call(this, index) === true ){
+            if(mutate === false || mutate == undefined){
+                news.push( this.data );
+            } else {
+                news.push( this.detach() );
+            }
+        }
+    });
+    
+    if(news.length > 0){
+        if(mutate === false || mutate == undefined){
+            newHook = new HookShot(news[0]);
+            for(i = 1, l = news.length; i < l; i++){
+                newHook = newHook.push( news[i] );
+            }
+        } else {
+            newHook = news[0];
+            for(i = 1, l = news.length; i < l; i++){
+                newHook = newHook.link( news[i] );
+            }
+        }
+    }
+    
+    return newHook;
 }
 HookShot.prototype.flatten = function(){
     var current = this.head
-		,arr = [];
+        ,arr = [];
     while(current){       
         arr.push(current.data);
         current = current.next;
     }
-	return arr;
+    return arr;
 }
-
-LN = HookShot;
