13b8773355c8d47a5e9887c3167158b8743e3a12
KirbySaysHi <kirbysayshi@gmail.com>
2010-11-27 13:52:33 -0500

    completely rewrote HookShot, using intermediary LongShot.

--- HookShot.js
+++ HookShot.js
@@ -1,138 +1,27 @@
-function HookShot(data, isHead){
-    this.head;
-    this.next;
-    this.prev;
-    this.data = data;
-    
-    if(isHead === true || isHead == undefined){ 
-        this.head = this; 
-        this.length = 1;
-    }
-}
-
-HookShot.prototype.length = function(){
-    return this.head.length;
-};
-HookShot.prototype.push = function(data){
-    var node = new HookShot(data, false);
-    this.next = node;
-    node.prev = this;
-    node.head = this.head;
-    this.head.length++;
-    return node;
-}
-HookShot.prototype.link = function(link){
-    this.next = link;
-    link.prev = this;
-    link.head = this.head;
-    this.head.length++;
-    return link;
-}
-HookShot.prototype.detach = function(){
-    var  prev = this.prev
-        ,next = this.next
-        ,length = this.head.length;
-    
-    // special case: reassign all the heads to the next in line...
-    if(this.head == this && next != undefined){
-        this.forEach(function(){
-            this.head = next;
-        });
-    }
-    
-    if(prev != undefined && prev.next == this) { prev.next = next; }
-    if(next != undefined && next.prev == this) { next.prev = prev; }
-    this.head.length = length-1;
-    
-    // set head to this and return
-    return this.head = this;
-}
-HookShot.prototype.get = function(i){
-    var current = this.head
-        ,count = 0
-        ,toCompare = ~~i;
-    while(current && current != this){ 
-        if(count === toCompare){
-            return current;
-        }
-        count++;
-        current = current.next;
-    }
-    return this;
-}
-HookShot.prototype.forEach = function(callback){
-    var current = this.head
-        ,count = 0;
-
-    while(current){       
-        if(callback.call(current, count) === false){
-            break;
-        }
-        count++;
-        current = current.next;
-    }
-    return this;
-}
-HookShot.prototype.clone = function(){
-    var current = new HookShot(this.data);
-    this.forEach(function(index){
-        if(index > 0){
-            current = current.push(this.data);
-        }
-    });
-    return current;
-}
-HookShot.prototype.where = function(conditionCallback, mutate){
-    var  news = []
-        ,newHook = new HookShot()
-        ,i = 0
-        ,l = 0;
-    
-    this.forEach(function(index){
-        if( conditionCallback.call(this, index) === true ){
-            if(mutate === false || mutate == undefined){
-                news.push( this.data );
-            } else {
-                news.push( this.detach() );
-            }
-        }
-    });
-    
-    if(news.length > 0){
-        if(mutate === false || mutate == undefined){
-            newHook = new HookShot(news[0]);
-            for(i = 1, l = news.length; i < l; i++){
-                newHook = newHook.push( news[i] );
-            }
-        } else {
-            newHook = news[0];
-            for(i = 1, l = news.length; i < l; i++){
-                newHook = newHook.link( news[i] );
-            }
-        }
-    }
-    
-    return newHook;
-}
-HookShot.prototype.flatten = function(){
-    var current = this.head
-        ,arr = [];
-    while(current){       
-        arr.push(current.data);
-        current = current.next;
-    }
-    return arr;
-}
-
 ///////////////////////////////////////////////////////////////////////////////
 
-function LongShot(data){
-	this.head = new LongShot.prototype.Link(data, this);
+/**
+ * Creates a new instance of a HookShot linked list. Each
+ * element is a Link object, which wraps the actual value.
+ *
+ * @param  Object value required
+ * @return	HookShot
+ * @constructor
+ */
+function HookShot(value){
+	this.head = new HookShot.prototype.Link(value, this);
 	this.current = this.head;
 	this.last = this.head;
 	this.length = 1;
 }
-LongShot.prototype.next = function(){
+/**
+ * Moves the internal pointer to the next element, and 
+ * returns that element if there is one, or false if 
+ * there is not.
+ *
+ * @return  Link/false   Link
+ */
+HookShot.prototype.next = function(){
 	this.current = this.current.next;
 	if(this.current == undefined){
 		return false;
@@ -140,18 +29,37 @@ LongShot.prototype.next = function(){
 		return this.current;
 	}
 }
-LongShot.prototype.reset = function(){
+/**
+ * Resets the internal pointer to the first element
+ * (Link) and returns that element.
+ *
+ * @return  void   the "head" Link
+ */
+HookShot.prototype.reset = function(){
 	return this.current = this.head;
 }
-LongShot.prototype.push = function(data){
-	var link = new LongShot.prototype.Link(data, this);
+/**
+ * Adds a new object (Link) to the end of the list, and 
+ * returns the list.
+ *
+ * @param  Object value  the new object to add
+ * @return  HookShot	this list
+ */
+HookShot.prototype.push = function(value){
+	var link = new HookShot.prototype.Link(value, this);
 	this.last.next = link;
 	link.prev = this.last;
 	this.last = link;
 	this.length += 1;
 	return this;
 }
-LongShot.prototype.pop = function(){
+/**
+ * Removes the last Link from the list and sets the internal
+ * pointer, if necessary to the new last element of the list.
+ *
+ * @return  Link   the former last Link of the list
+ */
+HookShot.prototype.pop = function(){
 	var last = this.last;
 	last.prev.next = undefined;
 	this.length -= 1;
@@ -161,7 +69,15 @@ LongShot.prototype.pop = function(){
 	}
 	return last;
 }
-LongShot.prototype.detachCurrent = function(){
+/**
+ * Removes the Link that the internal pointer is currently
+ * pointing at, and joins the surrounding links of necessary.
+ * The Link's connection to its former previous and next peers
+ * is left intact, but it's wielder is not.
+ *
+ * @return  Link   the former current Link
+ */
+HookShot.prototype.detachCurrent = function(){
 	var l = this.current,
 		p = l.prev,
 		n = l.next;
@@ -184,18 +100,27 @@ LongShot.prototype.detachCurrent = function(){
 	this.length -= 1;	
 	return l;
 }
-LongShot.prototype.join = function(thing){
-	if(thing instanceof LongShot.prototype.Link){
+/**
+ * Can take a Link or HookShot and join it to the end of the
+ * HookShot instance, efectively creating one list. The thing is 
+ * actually cloned, so it is left intact. If a simple object is passed
+ * in, it is just pushed onto the end of the list as a new Link.
+ *
+ * @param  HookShot/Link/Object thing  something to add onto the end of the list
+ * @return  HookShot   the original list with the new additions
+ */
+HookShot.prototype.join = function(thing){
+	if(thing instanceof HookShot.prototype.Link){
 		var last = this.last;
 		thing.prev = last;
 		last.next = thing;
 		this.last = thing;
 		this.length += 1;
 		return this;
-	} else if(thing instanceof LongShot) {
+	} else if(thing instanceof HookShot) {
 		var el = thing.reset(), count = 0;
 		while(el){
-			this.push(el.data);
+			this.push(el.value);
 			count += 1;
 			el = thing.next();
 		}
@@ -204,10 +129,19 @@ LongShot.prototype.join = function(thing){
 		return this;
 	}
 }
-LongShot.prototype.forEach = function(callback){	
+/**
+ * Resets the internal pointer, and iterates through each element
+ * of the list, calling the callback for each element. The callback is 
+ * passed two parameters, the "value" of each link, and the count of the 
+ * current link. "this" inside the callback is the Link itself.
+ *
+ * @param  Function callback  the function to call on each Link
+ * @return  HookShot   this list
+ */
+HookShot.prototype.forEach = function(callback){	
 	var el = this.reset(), count = 0;
 	while(el){
-		if(callback.call(el, el.data, count) === false){
+		if(callback.call(el, el.value, count) === false){
 			break;
 		}
 		count += 1;
@@ -215,15 +149,27 @@ LongShot.prototype.forEach = function(callback){
 	}
 	return this;
 }
-LongShot.prototype.flatten = function(){
+/**
+ * Converts the list to an array. The original list is left 
+ * untouched.
+ *
+ * @return  Array   The flattened list as an array
+ */
+HookShot.prototype.flatten = function(){
 	var arr = [], el = this.reset();
 	while(el){
-		arr.push(el.data);
+		arr.push(el.value);
 		el = this.next();
 	}
 	return arr;
 }
-LongShot.prototype.getAt = function(i){  
+/**
+ * Gets the value of the Link at the given index.
+ *
+ * @param  integer i  The index of the value to retrieve
+ * @return  mixed/HookShot   whatever the value is, or the list if the index is invalid
+ */
+HookShot.prototype.getAt = function(i){  
 	var  el = this.reset()
         ,count = 0
         ,toCompare = ~~i;
@@ -231,33 +177,49 @@ LongShot.prototype.getAt = function(i){
     while(el){ 
         if(count === toCompare){
 			this.reset();
-            return el.data;
+            return el.value;
         }
 		el = this.next();
         count++;
     }
     return this;
 }
-LongShot.prototype.clone = function(){
+/**
+ * Creates a new HookShot from another. Depending on the target value,
+ * the new HookShot's Links may just be references to the target value.
+ * i.e., this does not do a deep clone.
+ *
+ * @return  HookShot   the new list
+ */
+HookShot.prototype.clone = function(){
 	var el = this.reset(), clone;
 	while(el){
 		if(clone == undefined){
-			clone = new LongShot(el.data);
+			clone = new HookShot(el.value);
 		} else {
-			clone.push(el.data);
+			clone.push(el.value);
 		}
 		el = this.next();
 	}
 	return clone;
 }
-LongShot.prototype.where = function(conditionalCallback){
+/**
+ * Creates a new list from an existing one by iterating and filtering 
+ * elements. If the callback returns true, the element is added to a 
+ * new list. If false, the element is not added. The original list is
+ * left untouched. 
+ *
+ * @param  Function conditionalCallback  the function to test each element
+ * @return  HookShot   the new list of qualifying Links
+ */
+HookShot.prototype.where = function(conditionalCallback){
 	var el = this.reset(), count = 0, news;
 	while(el){
-		if(conditionalCallback.call(el, el.data, count) === true){
+		if(conditionalCallback.call(el, el.value, count) === true){
 			if(news == undefined){
-				news = new LongShot(el.data);
+				news = new HookShot(el.value);
 			} else {
-				news.push(el.data)
+				news.push(el.value)
 			}
 		}
 		count += 1;
@@ -265,14 +227,21 @@ LongShot.prototype.where = function(conditionalCallback){
 	}
 	return news;
 }
-LongShot.prototype.select = function(conditionalCallback){
+/**
+ * Same as where, but mutates the original list by removing Links
+ * that match the condition and placing them into a new list.
+ *
+ * @param  Function conditionalCallback  the function to test each element
+ * @return  HookShot   the new list of qualifying Links
+ */
+HookShot.prototype.select = function(conditionalCallback){
 	var el = this.reset(), count = 0, news;
 	while(el){
-		if(conditionalCallback.call(el, el.data, count) === true){
+		if(conditionalCallback.call(el, el.value, count) === true){
 			if(news == undefined){
-				news = new LongShot(el.data);
+				news = new HookShot(el.value);
 			} else {
-				news.push(el.data)
+				news.push(el.value)
 			}
 			this.detachCurrent();
 			el = this.current;
@@ -284,9 +253,17 @@ LongShot.prototype.select = function(conditionalCallback){
 	return news;
 }
 
-LongShot.prototype.Link = function(data, wielder){
+/**
+ * Mostly used internally, this is used as a wrapper for each element of
+ * a HookShot, which is accessed by its value property.
+ *
+ * @param  Object value  the value this link should hold
+ * @param  HookShot  wielder  the parent list of this link
+ * @return  void   
+ */
+HookShot.prototype.Link = function(value, wielder){
 	this.next;
     this.prev;
-    this.data = data;
+    this.value = value;
 	this.wielder = wielder;
 }
